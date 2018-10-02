2b34d5f8bbe44b4559ea64ed070fde16213d11b1
Tom <tom.claes82@gmail.com>
2018-10-02 12:29:08 +0200

    done

--- frontend/src/app/contact-create/contact-create.component.ts
+++ frontend/src/app/contact-create/contact-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-contact-create',
@@ -12,20 +13,27 @@ export class ContactCreateComponent implements OnInit {
     firstName: new FormControl("", [Validators.required]),
     lastName: new FormControl("", [Validators.required]),
     email: new FormControl("", [Validators.required,
-    Validators.pattern('[a-zA-z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z+]')]),
+    Validators.pattern('[a-zA-z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z]+')]),
     phone: new FormControl("", [Validators.pattern('[0-9]+')]),
     city: new FormControl(),
     country: new FormControl(),
     title: new FormControl()
   });
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
   }
 
   public onSubmit() {
     console.log(this.contactForm.value);
+    this.apiService.createContact(this.contactForm.value).subscribe( res => {
+      alert('Success');
+      this.contactForm.reset();
+    }, err => {
+      alert('Error');
+      this.contactForm.reset();
+    });
   }
 
   get email() {
