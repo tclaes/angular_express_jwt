c64f1dca136a3e715c784dd2651bc6617fda7561
Tom <tom.claes82@gmail.com>
2018-10-02 12:21:01 +0200

    layout

--- frontend/src/app/contact-create/contact-create.component.ts
+++ frontend/src/app/contact-create/contact-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-create',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactCreateComponent implements OnInit {
 
+  contactForm = new FormGroup({
+    firstName: new FormControl("", [Validators.required]),
+    lastName: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required,
+    Validators.pattern('[a-zA-z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z+]')]),
+    phone: new FormControl("", [Validators.pattern('[0-9]+')]),
+    city: new FormControl(),
+    country: new FormControl(),
+    title: new FormControl()
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onSubmit() {
+    console.log(this.contactForm.value);
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get phone() {
+    return this.contactForm.get('phone');
   }
 
 }
